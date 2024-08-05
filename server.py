ba74763aae07fc784dffdc90a35434c86f79d42b
Sufaqua <106598361+Sufaqua@users.noreply.github.com>
2024-08-05 01:28:50 +0000

    Mundo guardado 2024-08-05

--- server.py
+++ server.py
@@ -12,7 +12,7 @@
 # au		      Australia (Sydney)
 # eu		      Europa (Frankfurt)
 # in		      India (Mumbai)
-# jp		      Japón (Tokyo)
+# jp		      Japón (Tokyo) 
 # sa		      Sudamérica (São Paulo)
 # us		      Estados unidos (Ohio)
 # us-cal-1	      Estados unidos (California)
